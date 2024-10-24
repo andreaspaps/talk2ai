5bb7e54bb2cb877ee7882ea9191aeb57b53aacf8
andreaspaps <96740937+andreaspaps@users.noreply.github.com>
2024-10-24 17:14:48 +0300

    Create routing.ts

---
+++ src/app/pages/routing.ts
@@ -0,0 +1,25 @@
+const Routing: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+  {
+    path: 'builder',
+    loadChildren: () =>
+      import('./builder/builder.module').then((m) => m.BuilderModule),
+  },
+  {
+    path: 'crafted/pages/profile',
+    loadChildren: () =>
+      import('../modules/profile/profile.module').then((m) => m.ProfileModule),
+  },
+  {
+    path: 'crafted/account',
+    loadChildren: () =>
+      import('../modules/account/account.module').then((m) => m.AccountModule),
+  },
+  +{
+  +   path: 'privacy-policy', // <= Page URL
+  +   component: MyPageComponent // <= Page component registration
+  +},
