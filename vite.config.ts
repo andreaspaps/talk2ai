2679346bb05218632d6e5a1a45ea4e5f28cbe647
andreaspaps <96740937+andreaspaps@users.noreply.github.com>
2024-10-25 12:04:51 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/privacy.html',  // Copy from `src`
+          dest: ''                  // Copy to the root of `dist`
+        }
+      ]
+    })
+  ]
 });
