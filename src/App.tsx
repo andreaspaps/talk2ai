5428e860ebb4aa4570d0f0e9d3c3f91ea8cd127c
andreaspaps <96740937+andreaspaps@users.noreply.github.com>
2024-10-25 14:32:20 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -97,7 +97,7 @@ function App() {
             <div className="flex-1 flex justify-center items-center animate-fade-in lg:pr-4">
               <div className="relative w-[320px] h-[640px] rounded-[3rem] bg-gray-800 p-4 shadow-xl">
                 {/* Phone Frame */}
-                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-32 h-6 bg-black rounded-b-2xl" />
+                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-8 bg-black rounded-b-lg" />
                 
                 {/* Screen Content */}
                 <div className="relative w-full h-full overflow-hidden rounded-[2rem] bg-white">
