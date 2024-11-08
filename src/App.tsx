1ee680491f38fa61ed9a1b4327b0754ad2b1276d
andreaspaps <96740937+andreaspaps@users.noreply.github.com>
2024-11-09 00:53:00 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -209,7 +209,7 @@ function App() {
         </div>
       </div>
       {/* Text Content Section */}
-<div className="py-20 bg-gray-800">
+<div className="py-20">
   <div className="container mx-auto px-4 md:px-6 lg:px-8 max-w-7xl text-gray-300 overflow-hidden">
     {/* Grid Layout */}
     <div className="grid grid-cols-1 md:grid-cols-2 gap-14 text-lg md:text-2lg">
