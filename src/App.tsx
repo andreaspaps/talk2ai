a8b6ed2629b37a0332f413e910c901a22aa3b5a2
andreaspaps <96740937+andreaspaps@users.noreply.github.com>
2024-12-15 18:10:37 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prev) => (prev + 1) % sliderImages.length);
-    }, 4000);
+    }, 5000);
 
     return () => clearInterval(interval);
   }, []);
