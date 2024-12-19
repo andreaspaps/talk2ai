c55a42c40928a1c722f39c9f5b1c88b1e625e6c0
andreaspaps <96740937+andreaspaps@users.noreply.github.com>
2024-12-19 15:56:27 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -104,6 +104,9 @@ function App() {
     />
   </a>
 </div>
+<div className="mt-4 animate-fade-in mb-5 flex justify-center lg:justify-start">
+  <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="freeaichat" data-color="#5F7FFF" data-emoji="☕"  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
+</div>
 
             </div>
 
